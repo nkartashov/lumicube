deb506f7d4c1587d67a3287a757b3facca2b3a60
Nick Kartashov <snailandmail@gmail.com>
2022-09-30 20:52:46 +0100

    Refactor, add time modes, add pictures for night mode

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from enum import Enum
 import time
+from datetime import datetime
 
 
 class MODE(Enum):
     LAVA = 0
     MONOCHROME = 1
+    RAINBOW = 2
+
+
+class TIME_MODE(Enum):
+    DAY = 0
+    NIGHT = 1
 
 
 STEP_DELAY = 0.025
@@ -13,6 +20,7 @@ STEP_DELAY = 0.025
 class State:
     def __init__(self):
         self._mode = MODE.LAVA
+        self._time_mode = TIME_MODE.DAY
         self._time_step = 0
 
     def time_step(self):
@@ -21,8 +29,14 @@ class State:
     def mode(self) -> MODE:
         return self._mode
 
+    def time_mode(self) -> TIME_MODE:
+        return self._time_mode
+
     def set_mode(self, mode: MODE):
         self._mode = mode
+
+    def set_time_mode(self, time_mode: TIME_MODE):
+        self._time_mode = time_mode
 
     def next_step(self):
         self._time_step += 1
@@ -47,29 +61,129 @@ def paint_cube(t):
     display.set_3d(colours)
 
 
-def lava_lamp_main(step):
-    paint_cube(step)
+def draw_moon():
+    w = white
+    g = grey
+    moon = [
+        [0, 0, w, w, w, w, g, 0],
+        [0, w, w, w, w, w, w, g],
+        [w, w, w, w, g, 0, 0, 0],
+        [w, w, w, g, 0, 0, 0, 0],
+        [w, w, w, g, 0, 0, 0, 0],
+        [w, w, w, w, g, 0, 0, 0],
+        [0, w, w, w, w, w, w, g],
+        [0, 0, w, w, w, w, g, 0],
+    ]
+    return moon
 
 
-def monochrome_main(step):
-    hue = (step % 100) / 100
-    display.set_all(hsv_colour(hue, 1, 1))
+def draw_sparkles():
+    w = white
+    p = pink
+    sparkles = [
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, p, 0, 0, 0, 0, 0],
+        [0, p, w, p, 0, 0, 0, 0],
+        [0, 0, p, 0, 0, p, 0, 0],
+        [0, 0, 0, 0, p, w, p, 0],
+        [0, 0, p, 0, 0, p, 0, 0],
+        [0, p, w, p, 0, 0, 0, 0],
+        [0, 0, p, 0, 0, 0, 0, 0],
+    ]
+    return sparkles
 
 
-def run_mode_switcher(state: State):
-    mode_to_set = buttons.top_pressed_count % len(MODE)
-    state.set_mode(MODE(mode_to_set))
+def draw_wizard_fire():
+    w = white
+    light_blue = hsv_colour(0.6, 0.4, 1)
+    blue = hsv_colour(0.6, 0.9, 1)
+    b = blue
+    l = light_blue
+    wizard_fire = [
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, l, 0, l, 0, 0, 0, 0],
+        [l, 0, l, w, l, 0, 0, 0],
+        [0, 0, l, l, w, b, 0, 0],
+        [0, l, l, w, l, l, b, 0],
+        [0, b, w, w, w, l, b, 0],
+        [0, b, l, w, w, b, 0, 0],
+        [0, 0, b, b, b, 0, 0, 0],
+    ]
+    return wizard_fire
+
+
+def draw_heart():
+    r = red
+    o = orange
+    w = white
+    heart = [
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, r, r, 0, 0, r, r, 0],
+        [r, w, o, r, r, r, r, r],
+        [r, o, r, r, r, r, o, r],
+        [0, r, r, r, r, o, r, 0],
+        [0, 0, r, r, r, r, 0, 0],
+        [0, 0, 0, r, r, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+    ]
+    return heart
+
+
+class MainLoop:
+    def __init__(self):
+        self._state = State()
+
+    def _lava_lamp_main(self):
+        paint_cube(self._state.time_step())
+
+    def _monochrome_main(self):
+        hue = (self._state.time_step() % 100) / 100
+        display.set_all(hsv_colour(hue, 1, 1))
+
+    def _rainbow_main(self):
+        leds = {}
+        for y in range(16):
+            for x in range(16):
+                if x < 8 or y < 8:
+                    leds[x, y] = hsv_colour((x + y) / 24, 1, 1)
+        display.set_leds(leds)
+
+    def _night_mode_main(self):
+        display.set_panel("left", draw_moon())
+        display.set_panel("right", draw_sparkles())
+        display.set_panel("top", draw_heart())
+
+    def run_mode_switcher(self):
+        mode_to_set = buttons.top_pressed_count % len(MODE)
+        self._state.set_mode(MODE(mode_to_set))
+        now = datetime.now()
+        if 9 < now.hour < 21:
+            self._state.set_time_mode(TIME_MODE.DAY)
+        else:
+            self._state.set_time_mode(TIME_MODE.NIGHT)
+
+    def run(self):
+        MODES_TO_HANDLER = {
+            MODE.LAVA: self._lava_lamp_main,
+            MODE.MONOCHROME: self._monochrome_main,
+            MODE.RAINBOW: self._rainbow_main,
+        }
+
+        while True:
+            if self._state.time_mode() == TIME_MODE.DAY:
+                try:
+                    MODES_TO_HANDLER[self._state.mode()]()
+                except KeyError:
+                    print(f"Unrecognized mode: {self._state.mode()}")
+            elif self._state.time_mode():
+                self._night_mode_main()
+            self.run_mode_switcher()
+            self._state.next_step()
 
 
 def main():
-    state = State()
-    while True:
-        if state.mode() == MODE.LAVA:
-            lava_lamp_main(state.time_step())
-        elif state.mode() == MODE.MONOCHROME:
-            monochrome_main(state.time_step())
-        run_mode_switcher(state)
-        state.next_step()
+    main_loop = MainLoop()
+    main_loop.run()
 
 
 main()
